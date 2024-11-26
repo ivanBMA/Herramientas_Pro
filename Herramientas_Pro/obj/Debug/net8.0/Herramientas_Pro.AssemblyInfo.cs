4e3454eed8fd920f6213d9eb2c9395dcf107286f
ivanBMA <97181101+ivanBMA@users.noreply.github.com>
2024-11-26 10:38:26 +0100

    Mañana parte 1
    Cambios de diseño visual de los INDEX

--- Herramientas_Pro/obj/Debug/net8.0/Herramientas_Pro.AssemblyInfo.cs
+++ Herramientas_Pro/obj/Debug/net8.0/Herramientas_Pro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Herramientas_Pro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc2628411e7bd0f39833b4996da30ca477daae16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bee903f873b5a4479952748067d784643ed12d52")]
 [assembly: System.Reflection.AssemblyProductAttribute("Herramientas_Pro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Herramientas_Pro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
